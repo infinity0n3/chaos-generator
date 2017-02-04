a9be0012f7d96738e5406a3eef4dd2c1496d4bae
Daniel Kesler <kesler.daniel@gmail.com>
2017-02-04 23:51:49 +0100

    - updated: work in progress for load from json

--- python/framework/qt5/filter.py
+++ python/framework/qt5/filter.py
@@ -13,13 +13,13 @@ tojson_map = {
 }
 
 fromjson_map = {
-	"QString" : "toString()",
-	"QLatin1String" : "{0}toString()",
-	"bool" : "{0}toBool()",
-	"int" : "{0}toInt()",
-	"unsigned" : "(unsigned){0}toInt()",
-	"double" : "{0}toDouble()",
-	"qint64" : "{0}toInt()",
+	"QString" : '{0}["{1}"].toString()',
+	"QLatin1String" : '{0}["{1}"].toString()',
+	"bool" : '{0}["{1}"].toBool()',
+	"int" : '{0}["{1}"].toInt()',
+	"unsigned" : '(unsigned){0}["{1}"].toInt()',
+	"double" : '{0}["{1}"].toDouble()',
+	"qint64" : '{0}["{1}"].toInt()',
 }
 
 def qt_to_jsonvalue(var, var_type='', prefix='', suffix='', serializer=''):
@@ -29,7 +29,7 @@ def qt_to_jsonvalue(var, var_type='', prefix='', suffix='', serializer=''):
 			var_name = prefix+var['name']+suffix
 			var_type = var['type']
 		else:
-			# error
+			print "Error (qt_to_jsonvalue): missing name or type"
 			pass
 	
 	elif isinstance(var, basestring):
@@ -41,8 +41,22 @@ def qt_to_jsonvalue(var, var_type='', prefix='', suffix='', serializer=''):
 		#~ return "/* !!! Direct conversion of type '{0}' to QJsonValue is not supported !!! */".format(var_type)
 		return cpp_attr_filter(var_name, var_type, "toJSONObject({0})".format(serializer))
 	
-def qt_from_jsonvalue(var, json_obj_name):
-	#print ">> qt_from_jsonvalue", var, json_obj_name
+def qt_from_jsonvalue(var, json_obj_name, prefix='', suffix='',  deserializer=''):
+	
+	if isinstance(var, dict):
+		if "name" in var and "type" in var:
+			var_name = prefix+var['name']+suffix
+			var_type = var['type']
+		else:
+			print "Error (qt_from_jsonvalue): missing name or type"
+			pass
+			
+	if var_type in tojson_map:
+		return fromjson_map[var_type].format(json_obj_name, var_name)
+	else:
+		#~ return "/* !!! Direct conversion of type '{0}' to QJsonValue is not supported !!! */".format(var_type)
+		return cpp_attr_filter(var_name, var_type, "fromJSONObject({0}, {1})".format(json_obj_name, deserializer))
+	
 	
 	return ""
 
